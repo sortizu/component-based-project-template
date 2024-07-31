96038be0f07c27734a823abc64c357a59f0b44d4
Sebastian Ortiz Urbai <sortizurbai@gmail.com>
2024-07-31 01:08:31 -0500

    Update state_template.gd

--- script_templates/state_template.gd
+++ script_templates/state_template.gd
@@ -1,9 +1,6 @@
 tool
 extends %BASE%
 
-# Define the needed components as global variables and use "get_component_from_actor" method
-# to show them as dependencies in editor
-
 func enter_state():
 	pass
 
