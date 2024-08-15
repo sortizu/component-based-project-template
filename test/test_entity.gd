9eb4b1ea00d90c608c78f811e64c71cb2d294acb
Sebastian Ortiz Urbai <sortizurbai@gmail.com>
2024-08-15 01:18:03 -0500

    LogFormatter methods moved to Logger

--- test/test_entity.gd
+++ test/test_entity.gd
@@ -1,13 +1,27 @@
 tool
 extends Entity
-
-func _enter_tree():
-	pass
-
+var thread: Thread = Thread.new()
+#var arr: Array = []
 func _ready():
 	OS.window_minimized= true
-	var logger: Logger = LogManager.get_logger("Entity")
 	var start_time: int = Time.get_ticks_usec()
-	for i in range(100):
-		logger.info("Testing this log ({name})",[self])
-	print("Log duration: %f microseconds"%((Time.get_ticks_usec()-start_time)/100000.0))
+#	thread.start(self,"print_loop",100000)
+	var logger: Logger = LogManager.get_logger("Entity")
+	logger.info("Testing this log ({name})",[self])
+#	thread.wait_to_finish()
+	print("Log duration: %f seconds"%((Time.get_ticks_usec()-start_time)/100000.0))
+
+func print_loop(loops: int):
+	var logger: Logger = LogManager.get_logger("Entity")
+	var arr: Array = []
+	for i in range(loops):
+#	while true:
+#		logger.info("Testing this log ({name})",[self])
+#		var newvar = Reference.new()
+		arr.append(1)
+#		arr.append({"one":1})
+#		arr.append(Reference.new())
+#		arr.append(Node.new())
+#		continue
+#		print("Testing this log ({name})")
+	print("finished"+str(arr.size()))
